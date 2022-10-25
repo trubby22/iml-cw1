1bc2cf9ab9383ed8421a81b900ea091280bf40fb
Sanjeet Chatterjee <sc1320@ic.ac.uk>
2022-10-25 15:27:14 +0100

    new code

--- decision_tree.py
+++ decision_tree.py
@@ -1,14 +1,12 @@
 import numpy as np
-from dataclasses import dataclass
 
-@dataclass
 class TreeNode:
-    attr: int
-    value: int
-    left: 'TreeNode' | 'LeafNode'
-    right: 'TreeNode' | 'LeafNode'
+    def __init__(self, attr, value, left=None, right=None):
+        self.attr = attr
+        self.value = value
+        self.left = left
+        self.right = right
 
-@dataclass
 class LeafNode:
     def __init__(self, attr):
         self.attr = attr
@@ -30,10 +28,7 @@ class DecisionTree:
             node.left, ldepth = self.decision_tree_learning(ldata_x, ldata_y, depth + 1)
             node.right, rdepth = self.decision_tree_learning(rdata_x, rdata_y, depth + 1)
             return node, max(ldepth, rdepth)
-
-    def find_split(self, train_x, train_y):
-        pass
-
+    
     def predict(self, test_x):
         curr = self.root
 
@@ -44,8 +39,65 @@ class DecisionTree:
                 curr = curr.right
 
         return curr.attr
-        
 
+    def find_split(self, train_x, train_y):
+        max_info_gain = 0
+        best_attr = None
+        best_label = None
+        best_lx = None
+        best_ly = None
+        best_rx = None
+        best_ry = None
+        for attr in range(train_x.shape[1]):
+            info_gain, label, lx, ly, rx, ry = self.find_split_attr(train_x, train_y, attr)
+            if info_gain > max_info_gain:
+                max_info_gain = info_gain
+                best_attr = attr
+                best_label = label
+                best_lx = lx
+                best_ly = ly
+                best_rx = rx
+                best_ry = ry
+        return best_label, best_attr, best_lx, best_ly, best_rx, best_ry
+
+    def find_split_attr(self, train_x, train_y, attr):
+        max_info_gain = 0
+        best_label = None
+        best_lx = None
+        best_ly = None
+        best_rx = None
+        best_ry = None
+        for label in range(2):
+            lx, ly, rx, ry = self.split_data(train_x, train_y, attr, label)
+            info_gain = self.information_gain(train_y, ly, ry)
+            if info_gain > max_info_gain:
+                max_info_gain = info_gain
+                best_label = label
+                best_lx = lx
+                best_ly = ly
+                best_rx = rx
+                best_ry = ry
+        return max_info_gain, best_label, best_lx, best_ly, best_rx, best_ry
+    
+    def split_data(self, train_x, train_y, attr, label):
+        left_x = train_x[train_x[:, attr] == label]
+        left_y = train_y[train_x[:, attr] == label]
+        right_x = train_x[train_x[:, attr] != label]
+        right_y = train_y[train_x[:, attr] != label]
+        return left_x, left_y, right_x, right_y
+
+    def information_gain(self, parent, left, right):
+        p = len(left) / len(parent)
+        return self.entropy(parent) - p * self.entropy(left) - (1 - p) * self.entropy(right)
+
+    def entropy(self, data):
+        if len(data) == 0:
+            return 0
+        p = np.sum(data) / len(data)
+        return -p * np.log2(p) - (1 - p) * np.log2(1 - p)
 
 if __name__ == '__main__':
-    pass
+    train_x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    train_y = np.array([0, 1, 1, 0])
+    dtc = DecisionTree()
+    dtc.decision_tree_learning(train_x, train_y)
